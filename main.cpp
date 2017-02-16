c468d8e53c3c9d6b154cb92a9809fb4455ba459f
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-02-16 17:07:30 -0500

    Cleanup

--- main.cpp
+++ main.cpp
@@ -11,13 +11,23 @@ const float keep(0.5f);
 const float mutateRate(0.1f);
 
 struct sample {
+ private:
   std::string code;
   unsigned int value;
   double fit;
 
+ public:
   sample();
 
   explicit sample(std::string code);
+
+  const std::string &getCode() const;
+
+  void setCode(const std::string &code);
+
+  unsigned int getValue() const;
+
+  double getFit() const;
 };
 
 void printSamples(const std::vector<sample> &samples);
@@ -47,17 +57,16 @@ int main() {
     // evaluate fitness function
     samples[i] = sample(temp);
   }
-  std::cout << "Sorted: " << std::endl;
+  std::cout << "Generation 0"<< std::endl;
   printSamples(samples);
-  std::sort(samples.begin(), samples.end(), [](sample a, sample b) { return b.fit < a.fit; });
-  std::cout << "Sorted: " << std::endl;
-  printSamples(samples);
-  crossover(samples);
-  std::cout << "Crossover: " << std::endl;
-  printSamples(samples);
-  mutate(samples);
-  std::cout << "Mutate: " << std::endl;
-  printSamples(samples);
+
+  for (auto k(0); k < 100; ++k) {
+    std::sort(samples.begin(), samples.end(), [](sample a, sample b) { return b.getFit() < a.getFit(); });
+    crossover(samples);
+    mutate(samples);
+    std::cout << "Generation " << k + 1 << std::endl;
+    printSamples(samples);
+  }
 
   return 0;
 }
@@ -69,12 +78,30 @@ sample::sample(std::string code) :
     value(toUnsignedInt(code)),
     fit(fitness(value)) {}
 
+const std::string &sample::getCode() const {
+  return code;
+}
+
+void sample::setCode(const std::string &code) {
+  sample::code = code;
+  value = toUnsignedInt(code);
+  fit = fitness(value);
+}
+
+unsigned int sample::getValue() const {
+  return value;
+}
+
+double sample::getFit() const {
+  return fit;
+}
+
 void printSamples(const std::vector<sample> &samples) {
   for (int i(0); i < samples.size(); ++i) {
     std::cout << "Sample " << i << ": \tcode("
-              << samples[i].code << "), \tvalue("
-              << samples[i].value << "), \tfit("
-              << samples[i].fit << ");" << std::endl;
+              << samples[i].getCode() << "), \tvalue("
+              << samples[i].getValue() << "), \tfit("
+              << samples[i].getFit() << ");" << std::endl;
   }
 }
 
@@ -97,8 +124,8 @@ void crossover(std::vector<sample>& samples) {
 
     int cut((rand() % candidateSize));
 
-    results.push_back(sample(one.code.substr(0, cut) + two.code.substr(cut, candidateSize - cut)));
-    results.push_back(sample(two.code.substr(0, cut) + one.code.substr(cut, candidateSize - cut)));
+    results.push_back(sample(one.getCode().substr(0, cut) + two.getCode().substr(cut, candidateSize - cut)));
+    results.push_back(sample(two.getCode().substr(0, cut) + one.getCode().substr(cut, candidateSize - cut)));
   }
   std::copy(results.begin(), results.end(), samples.begin());
   return;
@@ -109,7 +136,9 @@ void mutate(std::vector<sample> &samples) {
     if (((rand() % 10) / 10.0f) < mutateRate) {
       // choose element
       int index(rand() % candidateSize);
-      samples[i].code[index] = (samples[i].code[index] == '0' ? '1' : '0');
+      std::string code(samples[i].getCode());
+      code[index] = code[index] == '0' ? '1' : '0';
+      samples[i].setCode(code);
     }
   }
   return;
